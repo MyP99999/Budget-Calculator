14523ec9034e6eb0647e27f76c9a1ab55f2ac2d9
MyP99999 <matheosprofirescu@yahoo.com>
2022-11-07 19:48:23 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
+import Alert from './components/Alert';
+import Form from './components/Form';
+
+const initialExpenses = [
+  {id: 1, charge:"rent", amount: 1600},
+  {id: 2, charge:"car", amount: 5000},
+  {id: 3, charge:"tax", amount: 200}
+]
+
+console.log(initialExpenses)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MuieMuie</h1>
-      </header>
-    </div>
+    <>
+    <Alert></Alert>
+    <Form></Form>
+    <List></List>
+    </>
   );
 }
 
